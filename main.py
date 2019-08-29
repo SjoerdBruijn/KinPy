d7093a34e5a2bed6096e4733ca872377487eda2c
Sjoerd Bruijn <s.m.bruijn@gmail.com>
2019-08-29 15:02:29 +0200

    some quick changes.

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 21:43:42 2019
+
+@author: sjoerd
+"""
+
+
+import scipy.io
+import kinpy as kp
+
+x,y,z,fs = kp.readndf("testdata/TN000064.ndf")
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(x[:,17:19],z[:,17:19])
+ax.axis('equal')
+ax.set(title= 'foot trajectory',
+       ylabel = 'z pos [mm]',
+       xlabel = 'x pos [mm]')
+
+mat = scipy.io.loadmat('testdata/TN000076.mat')
+traj =mat['traj']
+
+com = kp.calc_combined_com(traj)
+
+kp.plot_3d(traj)
+    
+    
+    
+    
+    #ani = Player(fig, update, maxi=100,interval=20)
+    
+    #plt.show()
