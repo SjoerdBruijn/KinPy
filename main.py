880294c89f7221d088ab04050d36332d26b3f55c
NickKluft <Nick1@McNick.local>
2019-08-30 16:44:17 +0200

    added xyz2dat and its inverse, and the  prod_col function

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ Created on Wed Aug 21 21:43:42 2019
 import scipy.io
 import kinpy as kp
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.maltib
+
 
 x,y,z,fs = kp.readndf("testdata/TN000077.ndf")
 fig = plt.figure()
@@ -19,6 +22,13 @@ ax.axis('equal')
 ax.set(title= 'foot trajectory',
        ylabel = 'z pos [mm]',
        xlabel = 'x pos [mm]')
+
+dat = kp.xyz2dat(x,y,z)
+
+eye = np.eye(3,k=0)
+eye = eye.reshape([1,9])
+eye = np.r_[eye,eye,eye,eye,eye]
+C = kp.prod_col(dat[0:5:1,0:9:1],eye)
 
 mat = scipy.io.loadmat('testdata/TN000076.mat')
 traj =mat['traj']
