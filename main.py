df95e70e60814d0cfdbbb89bb28a1e8d94117f22
Sjoerd Bruijn <s.m.bruijn@gmail.com>
2019-09-06 16:09:42 +0200

    no message

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ settings['reference_trial_nr']     = 89;
 settings['cluster_pointer_nr']     = range(78,88); 
 settings['forceplate_pointers_nr'] = range(12,16);
 settings['pointerfilename']         ='testdata/full data set/RB-06114.RIG'
+settings['oldversion']=1
 # of course, in our setting, all is still based on 1-based indexing. 
 
 BLM=dict()
@@ -59,12 +60,22 @@ n_seg=len(settings['segments'])
 for i_seg in range(n_seg): 
     BLM['segment'][0,i_seg]=dict()
     BLM['segment'][0,i_seg]['segment_name']=settings['segments']['segment_name'][i_seg]
-    BLM['segment'][0,i_seg]['pointer_nr']=settings['segments']['pointer nr'][i_seg]# this needs to be converted to integers still
-    BLM['segment'][0,i_seg]['opto_kol']=settings['segments']['marker columns'][i_seg]# this needs to be converted to integers still
+    if settings['oldversion']==1:
+        tmp=settings['segments']['pointer nr'][i_seg]
+        BLM['segment'][0,i_seg]['pointer_nr']=range(int(tmp[0])-1,int(tmp[2])-1)  
+        tmp=settings['segments']['marker columns'][i_seg]
+        BLM['segment'][0,i_seg]['opto_kol']=range(int(tmp[0])-1,int(tmp[2])-1)    
+       #BLM['segment'][0,i_seg]['blmforaxis']=int(settings['segments']['BLM from segments'][i_seg])-1  
+    else:
+        tmp=settings['segments']['pointer nr'][i_seg]
+        BLM['segment'][0,i_seg]['pointer_nr']=range(int(tmp[0]),int(tmp[2]))  
+        tmp=settings['segments']['marker columns'][i_seg]
+        BLM['segment'][0,i_seg]['opto_kol']=range(int(tmp[0]),int(tmp[2]))    
+       # BLM['segment'][0,i_seg]['blmforaxis']=settings['segments']['BLM from segments'][i_seg]  
+
     BLM['segment'][0,i_seg]['side']=settings['segments']['side'][i_seg]
     BLM['segment'][0,i_seg]['circumference']=settings['segments']['circumference'][i_seg]
     BLM['segment'][0,i_seg]['gender']=settings['segments']['gender'][i_seg]
-    BLM['segment'][0,i_seg]['blmforaxis']=settings['segments']['BLM from segments'][i_seg]    
     BLM['segment'][0,i_seg]['axis_function']=settings['segments']['ACS function'][i_seg]    
 
 pointer= kp.ImportPointerFile(settings['pointerfilename']) 
